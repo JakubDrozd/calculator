091a530af1593117965b4c04085138f88f15de9d
Veanty <plmulticraftpl@gmail.com>
2022-06-13 16:05:23 +0200

    I have to work something out on how to store numbers

--- script.js
+++ script.js
@@ -11,27 +11,51 @@ const operate = (operator, a, b) => {
   } else if (operator === "*") {
     return multiply(a, b);
   } else if (operator === "/") {
-    return divide(a, b);
+    if (b == 0) {
+      return "ERROR";
+    } else {
+      return divide(a, b);
+    }
   }
 };
+
+let firstNumber = 0;
+let secondNumber = 0;
+
+function pickOperator(e) {
+  if (!firstNumber) {
+    operator = e.target.value;
+    firstNumber = number;
+    display.textContent = 0;
+    number = 0;
+  } else {
+    secondNumber = number;
+  }
+}
 
 const display = document.querySelector(".display");
 display.textContent = "0";
 
-let actualNumber = 0;
+let operator = "";
+
+let number = 0;
 
 function insert(e) {
   if (display.textContent === "0") {
     display.textContent = e.target.value;
-    actualNumber = +display.textContent;
+    firstNumber = +display.textContent;
+    console.log(firstNumber);
   } else {
     display.textContent += e.target.value;
-    actualNumber = +display.textContent;
+    firstNumber = +display.textContent;
+    console.log(firstNumber);
   }
 }
 
 function clear() {
   display.textContent = "0";
+  number = 0;
+  console.log(number);
 }
 
 const seven = document
@@ -50,10 +74,21 @@ const three = document
   .querySelector(".three")
   .addEventListener("click", insert);
 
-const multiplyButton = document.querySelector(".multiply");
-const subtractButton = document.querySelector(".subtract");
-const addButton = document.querySelector(".add");
-const divideButton = document.querySelector(".divide");
+const multiplyButton = document
+  .querySelector(".multiply")
+  .addEventListener("click", pickOperator);
+const subtractButton = document
+  .querySelector(".subtract")
+  .addEventListener("click", pickOperator);
+const addButton = document
+  .querySelector(".add")
+  .addEventListener("click", pickOperator);
+const divideButton = document
+  .querySelector(".divide")
+  .addEventListener("click", pickOperator);
+const equalsButton = document
+  .querySelector(".equals")
+  .addEventListener("click", operate(operator, firstNumber, number));
 const clearButton = document
   .querySelector(".clear")
   .addEventListener("click", clear);
